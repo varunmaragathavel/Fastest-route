c9e97000aa5edfd3a4421ff1eb810acc517ab399
varunmaragathavel <162910229+varunmaragathavel@users.noreply.github.com>
2025-06-18 12:57:39 +0530

    functions.js

---
+++ function.js
@@ -0,0 +1,264 @@
+window.onload = function () {
+  // create a network
+  var curr_data;
+  var sz;
+  var container = document.getElementById("mynetwork");
+  var container2 = document.getElementById("mynetwork2");
+  var genNew = document.getElementById("generate-graph");
+  var solve = document.getElementById("solve");
+  var temptext = document.getElementById("temptext");
+  var temptext2 = document.getElementById("temptext2");
+  // initialise graph options
+  var options = {
+    edges: {
+      labelHighlightBold: true,
+      font: {
+        size: 20,
+      },
+    },
+    nodes: {
+      font: "12px arial red",
+      scaling: {
+        label: true,
+      },
+      shape: "icon",
+      icon: {
+        face: "FontAwesome",
+        code: "\uf015",
+        size: 40,
+        color: "blue",
+      },
+    },
+  };
+
+  var network = new vis.Network(container);
+  network.setOptions(options);
+  var network2 = new vis.Network(container2);
+  network2.setOptions(options);
+  const cities = [
+    "Chennai",
+    "Coimbatore",
+    "Trichy",
+    "Goa",
+    "Trivandrum",
+    "Mumbai",
+    "Hyderabad",
+    "Bangalore",
+    "Pune",
+    "Gurugram",
+  ];
+  function createData() {
+    sz = Math.floor(Math.random() * 8) + 3;
+
+    let nodes = [];
+    for (let i = 1; i <= sz; i++) {
+      nodes.push({ id: i, label: cities[i - 1] });
+    }
+    nodes = new vis.DataSet(nodes);
+
+    let edges = [];
+
+    for (let i = 2; i <= sz; i++) {
+      let neigh = i - Math.floor(Math.random() * Math.min(i - 1, 3) + 1);
+      edges.push({
+        type: 0,
+        from: i,
+        to: neigh,
+        color: "red",
+        label: String(Math.floor(Math.random() * 70) + 31),
+      });
+    }
+
+    src = Math.max(Math.floor(Math.random() * sz), 1);
+    dst = sz;
+
+    //this for loop is used to make a mix of bus and planes
+
+    for (let i = 1; i <= sz / 2; ) {
+      let n1 = Math.floor(Math.random() * sz) + 1;
+      let n2 = Math.floor(Math.random() * sz) + 1;
+      if (n1 != n2) {
+        //this if condition makes n2 as maximum value
+        if (n1 < n2) {
+          let tmp = n1;
+          n1 = n2;
+          n2 = tmp;
+        }
+        let works = 0;
+        //this loop checks if there is an edge from n1 to n2
+        for (let j = 0; j < edges.length; j++) {
+          if (edges[j]["from"] === n1 && edges[j]["to"] === n2) {
+            if (edges[j]["type"] === 0) works = 1;
+            else works = 2;
+          }
+        }
+
+        if (works <= 1) {
+          //if i<quarter the size of nodes, then adding type 0 is prioritized
+          //else adding type 1 is prioritized
+          if (works === 0 && i < sz / 4) {
+            edges.push({
+              type: 0,
+              from: n1,
+              to: n2,
+              color: "red",
+              label: String(Math.floor(Math.random() * 70) + 31),
+            });
+          } else {
+            edges.push({
+              type: 1,
+              from: n1,
+              to: n2,
+              color: "green",
+              label: String(Math.floor(Math.random() * 50) + 1),
+            });
+          }
+          // this is incremented only if an edge is added
+          i++;
+        }
+      }
+    }
+
+    let data = {
+      nodes: nodes,
+      edges: edges,
+    };
+    curr_data = data;
+  }
+
+  genNew.onclick = function () {
+    createData();
+    //the network(left) screen is updated with this data
+    network.setData(curr_data);
+    temptext2.innerText =
+      "Find least time path from " + cities[src - 1] + " to " + cities[dst - 1];
+    temptext.style.display = "inline";
+    temptext2.style.display = "inline";
+    container2.style.display = "none";
+  };
+
+  solve.onclick = function () {
+    temptext.style.display = "none";
+    temptext2.style.display = "none";
+    container2.style.display = "inline";
+    network2.setData(solveData(sz));
+  };
+
+  function dijkstra(graph, sz, src) {
+    let vis = Array(sz).fill(0);
+    let dist = [];
+    for (let i = 1; i <= sz; i++) dist.push([10000, -1]);
+    dist[src][0] = 0;
+
+    for (let i = 0; i < sz - 1; i++) {
+      let mn = -1;
+      for (let j = 0; j < sz; j++) {
+        if (vis[j] === 0) {
+          if (mn === -1 || dist[j][0] < dist[mn][0]) mn = j;
+        }
+      }
+
+      vis[mn] = 1;
+      for (let j in graph[mn]) {
+        console.log(j);
+        let edge = graph[mn][j];
+        if (vis[edge[0]] === 0 && dist[edge[0]][0] > dist[mn][0] + edge[1]) {
+          dist[edge[0]][0] = dist[mn][0] + edge[1];
+          dist[edge[0]][1] = mn;
+        }
+      }
+    }
+
+    return dist;
+  }
+
+  function solveData(sz) {
+    let data = curr_data;
+    let graph = [];
+    for (let i = 1; i <= sz; i++) {
+      graph.push([]);
+    }
+    //creating a graph only with bus (i.e, type 0)
+    for (let i = 0; i < data["edges"].length; i++) {
+      let edge = data["edges"][i];
+      if (edge["type"] === 1) continue;
+      graph[edge["to"] - 1].push([edge["from"] - 1, parseInt(edge["label"])]);
+      graph[edge["from"] - 1].push([edge["to"] - 1, parseInt(edge["label"])]);
+    }
+    //finding the shortest distance from src and also from dest
+    let dist1 = dijkstra(graph, sz, src - 1);
+    let dist2 = dijkstra(graph, sz, dst - 1);
+
+    let mn_dist = dist1[dst - 1][0];
+
+    let plane = 0;
+    let p1 = -1,
+      p2 = -1;
+    for (let pos in data["edges"]) {
+      let edge = data["edges"][pos];
+      if (edge["type"] === 1) {
+        let to = edge["to"] - 1;
+        let from = edge["from"] - 1;
+        let wght = parseInt(edge["label"]);
+        if (dist1[to][0] + wght + dist2[from][0] < mn_dist) {
+          plane = wght;
+          p1 = to;
+          p2 = from;
+          mn_dist = dist1[to][0] + wght + dist2[from][0];
+        }
+        if (dist2[to][0] + wght + dist1[from][0] < mn_dist) {
+          plane = wght;
+          p2 = to;
+          p1 = from;
+          mn_dist = dist2[to][0] + wght + dist1[from][0];
+        }
+      }
+    }
+
+    new_edges = [];
+    if (plane !== 0) {
+      new_edges.push({
+        arrows: { to: { enabled: true } },
+        from: p1 + 1,
+        to: p2 + 1,
+        color: "green",
+        label: String(plane),
+      });
+      pushEdges(dist1, p1, false);
+      pushEdges(dist2, p2, true);
+    } else {
+      pushEdges(dist1, dst - 1, false);
+    }
+    data = {
+      nodes: data["nodes"],
+      edges: new_edges,
+    };
+    return data;
+  }
+
+  function pushEdges(dist, curr, reverse) {
+    // tmp_edges = [];
+    while (dist[curr][0] != 0) {
+      let fm = dist[curr][1];
+      if (reverse)
+        new_edges.push({
+          arrows: { to: { enabled: true } },
+          from: curr + 1,
+          to: fm + 1,
+          color: "orange",
+          label: String(dist[curr][0] - dist[fm][0]),
+        });
+      else
+        new_edges.push({
+          arrows: { to: { enabled: true } },
+          from: fm + 1,
+          to: curr + 1,
+          color: "orange",
+          label: String(dist[curr][0] - dist[fm][0]),
+        });
+      curr = fm;
+    }
+  }
+
+  genNew.click();
+};
